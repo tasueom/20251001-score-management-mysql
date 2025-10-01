b363fd5330a7e17fecb319ece3713fefc6ac7d32
tasueom <tasueom@naver.com>
2025-10-01 16:10:27 +0900

    feat: 실행 시 index.html로 이동

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import db
 app = Flask(__name__)
 app.secret_key = "dev_key123"
 
+#실행 시
+@app.route("/")
+def index():
+    return ren("index.html", sno=session.get("sno"))
+
 #Flask 서버 실행
 if __name__ == "__main__":
     db.init_db()
