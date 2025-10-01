70300dc9f356e0eb21bb810d19a58b06b31a2fe1
tasueom <tasueom@naver.com>
2025-10-01 15:37:54 +0900

    feat: db 연결 및 테이블 생성

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template as ren, request, redirect, url_for, session
 import hashlib
+import db
 
 app = Flask(__name__)
+app.secret_key = "dev_key123"
 
 #Flask 서버 실행
 if __name__ == "__main__":
+    db.init_db()
     app.run(debug=True) #코드 수정 시 자동 반영 및 에러 디버깅
---
+++ db.py
@@ -0,0 +1,50 @@
+import mysql.connector
+
+# MySQL 기본 연결 설정 (데이터베이스를 지정하지 않고 접속)
+base_config = {
+    "host": "localhost",   # MySQL 서버 주소 (로컬)
+    "user": "root",        # MySQL 계정
+    "password": "1234"     # MySQL 비밀번호
+}
+
+# 사용할 데이터베이스 이름
+DB_NAME = "scoredb"
+
+# 커넥션과 커서 반환하는 함수
+def conn_db():
+    conn = mysql.connector.connect(database=DB_NAME, **base_config)
+    cur = conn.cursor()
+    return conn, cur
+
+def init_db():
+    # DB 생성 (없으면 자동 생성)
+    conn = mysql.connector.connect(**base_config)
+    cur = conn.cursor()
+    cur.execute(f"create database if not exists {DB_NAME} default character set utf8mb4")
+    conn.commit()
+    conn.close()
+    
+    # 생성된 DB에 접속해서 테이블 생성
+    conn, cur = conn_db()
+    cur.execute("""
+                create table students(
+                    sid varchar(20),
+                    ban int,
+                    sname varchar(40),
+                    password varchar(200)
+                )
+                """)
+    cur.execute("""
+                create table scores(
+                    sid varchar(20),
+                    kor int,
+                    eng int,
+                    mat int,
+                    tot int,
+                    average double,
+                    grade char(1),
+                    foreign key (sid) references students(sid)
+                )
+                """)
+    conn.commit()
+    conn.close()
